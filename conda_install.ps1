92a0cb008826494f42dfba49288c3497a645aaf5
Ryan Ashbaugh <rashbaugh23@gmail.com>
2023-07-16 00:06:00 -0400

    added powershell install script for windows

---
+++ conda_install.ps1
@@ -0,0 +1,27 @@
+# Specify the installer and installation directory
+$installer = "Miniconda3-latest-Windows-x86_64.exe"
+$install_dir = "$HOME\Miniconda3"
+$outfile_path = "$HOME\$installer"
+$repo_dir = "$HOME\visprog"
+
+# Download the Miniconda installer
+Invoke-WebRequest -Uri https://repo.anaconda.com/miniconda/$installer -OutFile $outfile_path
+
+# Install Miniconda
+Start-Process $outfile_path -ArgumentList "/InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=$install_dir" -Wait -NoNewWindow
+
+# Remove the installer
+Remove-Item -Path $outfile_path
+
+# Add the conda command to the current user's PATH environment variable
+$env:Path = "$env:Path;$install_dir;$install_dir\Scripts;$install_dir\Library\bin"
+
+# Refresh the current session for the changes to take effect
+RefreshEnv
+
+git clone https://github.com/RyanAshbaugh/VisProg.git $repo_dir
+
+chdir $repo_dir
+conda env create -f environment.yaml -y
+
+pip install python-magic-bin==0.4.14
