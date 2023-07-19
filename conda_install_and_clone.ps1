30dac8aca5ef6065422c55748dc04b30f0f20dd3
Ryan Ashbaugh <ashbau12@msu.edu>
2023-07-18 21:33:23 -0400

    new script for install and clone

--- conda_install_and_clone.ps1
+++ conda_install_and_clone.ps1
@@ -16,6 +16,8 @@ Remove-Item -Path $outfile_path
 # Add the conda command to the current user's PATH environment variable
 $env:Path = "$env:Path;$install_dir;$install_dir\Scripts;$install_dir\Library\bin"
 
+git clone https://github.com/RyanAshbaugh/VisProg.git $repo_dir
+
 chdir $repo_dir
 conda env create -f environment.yaml
 
